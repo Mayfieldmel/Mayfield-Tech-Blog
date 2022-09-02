8562d6e1d2d3895dcd54791cf817e9f019a2352d
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-02 13:35:03 -0400

    create withAuth

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+    //   res.redirect('/login');
+    console.log("redirect to login")
+    alert("redirect to login")
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
